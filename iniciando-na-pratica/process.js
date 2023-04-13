eff39ea12cf90ee98acf2a714062bb0dc0d736df
mairess <trabalho.maires@gmail.com>
2023-04-13 09:33:54 -0300

    update process.js

--- iniciando-na-pratica/process.js
+++ iniciando-na-pratica/process.js
@@ -1,8 +1,8 @@
 // lista de argumentos
-// console.log(process.argv)
+console.log(process.argv)
 
-const firstName = process.argv[2]
-const lastName = process.argv[3]
+// const firstName = process.argv[2]
+// const lastName = process.argv[3]
 // console.log('seu nome é', process.argv[2] + ' ' + process.argv[3] )
 
-console.log(`seu nome é ${firstName} ${lastName}`)
+// console.log(`seu nome é ${firstName} ${lastName}`)
