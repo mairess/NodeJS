6bc8fd2f17b9bb5df46888b43c5d39ae420d4752
mairess <trabalho.maires@gmail.com>
2023-04-13 12:52:12 -0300

    update index.js

--- best-of-me-app/index.js
+++ best-of-me-app/index.js
@@ -5,7 +5,18 @@ const questions = [
     'Quantas pessoas ajudei hoje',
 ]
 const ask = (index = 0) => {
-    process.stdout.write(questions[index] + "\n\n\n")
+    process.stdout.write("\n\n" + questions[index] + " > ")
 }
 
 ask()
+
+const answers = []
+process.stdin.on("data", data =>{
+    answers.push(data.toString().trim())
+    if (answers.length < questions.length) {
+        ask(answers.length)
+    } else {
+        console.log(answers)
+        process.exit()
+    } 
+})
