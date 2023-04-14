b56e49305690b3f6a4c49856ce4a72fb7909a574
mairess <trabalho.maires@gmail.com>
2023-04-14 15:28:02 -0300

    add ev.once()

--- events/events.js
+++ events/events.js
@@ -2,7 +2,8 @@ const {EventEmitter} = require('events')
 
 const ev = new EventEmitter()
 
-ev.on('saySomething', (message) => console.log('Eu ouvi você: ', message ))
+ev.once('saySomething', (message) => console.log('Eu ouvi você: ', message ))
+//ev.on('saySomething', (message) => console.log('Eu ouvi você: ', message ))
 
 ev.emit('saySomething', 'Maires')
 ev.emit('saySomething', 'João')
