211eca65ac4487da7f3ddeefd382f8c14c7dbb61
mairess <trabalho.maires@gmail.com>
2023-04-14 15:26:34 -0300

    update events.js

--- events/events.js
+++ events/events.js
@@ -2,4 +2,8 @@ const {EventEmitter} = require('events')
 
 const ev = new EventEmitter()
 
-ev.emit('saySomething')
+ev.on('saySomething', (message) => console.log('Eu ouvi você: ', message ))
+
+ev.emit('saySomething', 'Maires')
+ev.emit('saySomething', 'João')
+ev.emit('saySomething', 'Camila')
